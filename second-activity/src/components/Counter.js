b143fd1a3bcb8d85b7b830241a70e89eddd86bcb
peter parker <george.lagmay113@gmail.com>
2024-09-13 01:10:53 +0800

    Designed counter page, added google fonts

--- second-activity/src/components/Counter.js
+++ second-activity/src/components/Counter.js
@@ -1,28 +1,26 @@
-import '../components/Counter.css';
-import React, {useState} from 'react';
+import "../components/Counter.css";
+import React, { useState } from "react";
 
 function Counter() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return (
-        <div>
-            <h1>
-                You clicked {count} times
-            </h1>
+  return (
+    <div className="counterDiv">
+      <h1>You clicked {count} times</h1>
 
-            <button onClick={() => setCount(prev => prev + 1)}>
-                Increment
-            </button>
+      <button onClick={() => setCount((prev) => prev + 1)} id="buttonOne">
+        Increment
+      </button>
 
-            <button onClick={() => setCount(prev => prev - 1)}>
-                Decrement
-            </button>
+      <button onClick={() => setCount((prev) => prev - 1)} id="buttonTwo">
+        Decrement
+      </button>
 
-            <button onClick={() => setCount(0)}>
-                Reset
-            </button>
-        </div>
-    );
+      <button onClick={() => setCount(0)} id="buttonThree">
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default Counter;
