cedbcf0f1c384d5c183eb85127c8f4d72cb5f8bf
peter parker <george.lagmay113@gmail.com>
2024-09-16 12:40:52 +0800

    Updated contact page

--- second-activity/src/components/Contact.js
+++ second-activity/src/components/Contact.js
@@ -1,8 +1,44 @@
+import "../components/Contact.css";
+import React from "react";
+
 function Contact() {
   return (
-    <div>
-      <h1>Contact Page</h1>
-    </div>
+    <section className="contact">
+      <form>
+        <h2>Contact Form</h2>
+        <div className="input-box">
+          <label>Full Name</label>
+          <input
+            type="text"
+            className="field"
+            placeholder="Enter your name"
+            required
+          ></input>
+        </div>
+
+        <div className="input-box">
+          <label>Email Address</label>
+          <input
+            type="email"
+            className="field"
+            placeholder="Enter your email address"
+            required
+          ></input>
+        </div>
+
+        <div className="input-box">
+          <label>Your Message</label>
+          <textarea
+            name=""
+            id=""
+            className="field message"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+        </div>
+        <button type="submit">Send Message</button>
+      </form>
+    </section>
   );
 }
 
